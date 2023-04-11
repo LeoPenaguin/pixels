a34f995ebe7a1bb08ce6ae70670379eed1da9f24
leo-penaguin <penaguinleo@gmail.com>
2023-04-11 19:31:46 +0200

    [dev] template and structure

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,11 +3,16 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  </header>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/pixel-board">Pixel Board</RouterLink>
+  </nav>
 </template>
 
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+}
+</style>
---
+++ src/components/templates/PageContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-content">
+    <slot>Empty</slot>
+  </div>
+</template>
+
+<style lang="scss" scropped>
+.page-content {
+  padding: 1rem;
+}
+</style>
